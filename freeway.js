744da280bdfe499cc9f6a2599dc40b921e70b381
Estevão Coelho Kiel de Oliveira <estevaokiel@gmail.com>
2021-09-06 23:53:17 -0300

    Moving car1 and actor1.

--- freeway.js
+++ freeway.js
@@ -1,10 +1,30 @@
+//Background
 let imageRoad;
-let imageCar1;
-let imageCar2;
-let imageCar3;
-let imageActor1;
 
-function preload(){
+let widthCar = 50;
+let heightCar = 40;
+
+//Car 1
+let imageCar1;
+let xCar1 = 600;
+let yCar1 = 40;
+
+//Car 2
+let imageCar2;
+let xCar2;
+let yCar2;
+
+//Car 3
+let imageCar3;
+let xCar3;
+let yCar3;
+
+//Actor 1
+let imageActor1;
+let xActor1 = 100;
+let yActor1 = 366;
+
+function preload() {
   imageRoad = loadImage("images/road.png");
   imageCar1 = loadImage("images/car1.png");
   imageCar2 = loadImage("images/car2.png");
@@ -18,6 +38,33 @@ function setup() {
 
 function draw() {
   background(imageRoad);
-  image(imageActor1, 100, 366, 30, 30);
-  image(imageCar1, 420, 40, 50, 40);
+  showActor1();
+  moveActor1();
+  showCar1();
+  moveCar1();
+}
+
+function showActor1() {
+  image(imageActor1, xActor1, yActor1, 30, 30);
+}
+
+function showCar1() {
+  image(imageCar1, xCar1, yCar1, widthCar, heightCar);
+}
+
+function moveActor1() {
+  if(keyIsDown(UP_ARROW)) {
+     yActor1 -= 3;
+  }
+  
+  if(keyIsDown(DOWN_ARROW)) {
+     yActor1 += 3;
+  }
+}
+
+function moveCar1() {
+  xCar1 -= 2;
+  if (xCar1 == - widthCar){
+    xCar1 = 600;
+  }
 }
